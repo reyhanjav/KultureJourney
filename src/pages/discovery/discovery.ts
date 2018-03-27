0aa5dd5a492aeb739fa452ca5fc2a325fec44034
reyhanjav <reyhan.qartem@gmail.com>
2018-03-27 20:58:01 +0700

    update login google browser

--- src/pages/discovery/discovery.ts
+++ src/pages/discovery/discovery.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DiscoveryReadPage } from '../discovery-read/discovery-read';
 
 @Component({
   selector: 'page-discovery',
@@ -9,5 +10,7 @@ export class DiscoveryPage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  goToDiscoveryRead(discovery: any) {
+    this.navCtrl.push(DiscoveryReadPage);
+  }
 }
